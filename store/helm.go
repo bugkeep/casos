9119d0d8dfe9ae146a3a41b3ee24cb3f319c3eca
bugkeep <1921817430@qq.com>
2026-07-23 10:52:20 +0800

    fix: reuse implicitly latest helm images

--- store/helm.go
+++ store/helm.go
@@ -1214,6 +1214,7 @@ func InstallHelmChart(cfg *rest.Config, releaseName, namespace, chartName, repoU
 	install.CreateNamespace = true
 	install.Wait = true
 	install.Timeout = helmInstallTimeout
+	install.PostRenderer = localImagePullPolicyPostRenderer{}
 
 	_, err = install.Run(ch, vals)
 	if err != nil {
@@ -1305,6 +1306,7 @@ func InstallHelmChartStream(ctx context.Context, lifecycle HelmInstallLifecycle,
 		install.CreateNamespace = true
 		install.Wait = true
 		install.Timeout = helmInstallTimeout
+		install.PostRenderer = localImagePullPolicyPostRenderer{}
 		if _, err = install.RunWithContext(installCtx, helmChart, vals); err != nil {
 			for _, line := range helmReleaseDiagnostics(installCtx, cfg, releaseName, namespace) {
 				send(line)
@@ -1350,6 +1352,7 @@ func UpgradeHelmRelease(cfg *rest.Config, releaseName, namespace, chartName, rep
 	upgrade.Namespace = namespace
 	upgrade.Wait = true
 	upgrade.Timeout = helmOperationTimeout
+	upgrade.PostRenderer = localImagePullPolicyPostRenderer{}
 
 	_, err = upgrade.Run(releaseName, ch, vals)
 	return err
